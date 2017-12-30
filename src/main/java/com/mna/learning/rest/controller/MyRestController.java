f0116a34b501537ccc595f67aaad12de2b2b9f34
Maha Naji <maha@mbp-de-maha.home>
2017-12-30 12:12:39 +0100

    First Init

---
+++ src/main/java/com/mna/learning/rest/controller/MyRestController.java
@@ -0,0 +1,13 @@
+package com.mna.learning.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+    @RequestMapping("/ping")
+    public String ping(){
+        return "Poooooooong !!!";
+    }
+}
